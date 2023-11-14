3f88ab1f988509d13cbba5292a793d51fe3a1625
Ramy Ouda <ramyouda@gmail.com>
2023-11-13 19:46:53 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,52 @@
+// Access DOM
+const allRatingElements = document.querySelectorAll(".rating");
+const cardFrontElement = document.querySelector(".card-front");
+const cardBackElement = document.querySelector(".card-back");
+const submitButtonElement = document.querySelector(".submit-button");
+const ratingSelectionElement = document.querySelector(".rating-selection");
+
+// Initialize event listeners when page loads
+window.onload = initializeEventListeners;
+
+// Declare global variables
+let selectedRating = null;
+let previousRatingElement = null;
+
+// Initialize rating and submit events
+function initializeEventListeners() {
+  [...allRatingElements].forEach((rating) =>
+    rating.addEventListener("click", onRatingClicked)
+  );
+
+  submitButtonElement.addEventListener("click", onSubmit);
+}
+
+// Handle rating click events
+function onRatingClicked(event) {
+  const element = event.target;
+  selectedRating = element.innerText;
+
+  if (previousRatingElement) {
+    previousRatingElement.classList.remove("selected");
+
+    if (element === previousRatingElement) {
+      previousRatingElement = null;
+      return;
+    }
+  }
+
+  element.classList.add("selected");
+  previousRatingElement = element;
+}
+
+// Handle submit event
+function onSubmit() {
+  if (selectedRating) {
+    cardFrontElement.style.display = "none";
+    cardBackElement.style.display = "flex";
+
+    ratingSelectionElement.innerText = `You selected ${selectedRating} out of 5`;
+  } else {
+    alert("Please select a rating before submitting.");
+  }
+}
